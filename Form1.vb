8fecbd4a5bbb4c92c68918ecaf8e14625575e991
burny02 <d.burnside@retroscreen.com>
2016-05-06 17:03:51 +0100

    06May2016
    Initial Design. Set up Rota Calendar; Code to loop through all and fetch
    
    Signed-off-by: burny02 <d.burnside@retroscreen.com>

--- Form1.vb
+++ Form1.vb
@@ -58,6 +58,7 @@
             i += 1
         Loop
 
+
         i = 0
         Do While i < NumberOfMonthDays
             Dim TempCtl As Control() = Controls.Find("DataGridView" & FirstGridNumber + i, True)
@@ -67,6 +68,7 @@
             TempDGV.Columns(0).HeaderText = Format(CurDate, "dd-MMM")
             TempDGV.Visible = True
             i += 1
+
         Loop
 
 
@@ -79,6 +81,6 @@
     End Sub
 
     Private Sub DataGridView7_DoubleClick(sender As Object, e As EventArgs) Handles DataGridView9.DoubleClick, DataGridView8.DoubleClick, DataGridView7.DoubleClick, DataGridView6.DoubleClick, DataGridView5.DoubleClick, DataGridView42.DoubleClick, DataGridView41.DoubleClick, DataGridView40.DoubleClick, DataGridView4.DoubleClick, DataGridView39.DoubleClick, DataGridView38.DoubleClick, DataGridView37.DoubleClick, DataGridView36.DoubleClick, DataGridView35.DoubleClick, DataGridView34.DoubleClick, DataGridView33.DoubleClick, DataGridView32.DoubleClick, DataGridView31.DoubleClick, DataGridView30.DoubleClick, DataGridView3.DoubleClick, DataGridView29.DoubleClick, DataGridView28.DoubleClick, DataGridView27.DoubleClick, DataGridView26.DoubleClick, DataGridView25.DoubleClick, DataGridView24.DoubleClick, DataGridView23.DoubleClick, DataGridView22.DoubleClick, DataGridView21.DoubleClick, DataGridView20.DoubleClick, DataGridView2.DoubleClick, DataGridView19.DoubleClick, DataGridView18.DoubleClick, DataGridView17.DoubleClick, DataGridView16.DoubleClick, DataGridView15.DoubleClick, DataGridView14.DoubleClick, DataGridView13.DoubleClick, DataGridView12.DoubleClick, DataGridView11.DoubleClick, DataGridView10.DoubleClick, DataGridView1.DoubleClick
-        MsgBox("ok")
+        MsgBox("ok2")
     End Sub
 End Class
---
+++ Variables.vb
@@ -0,0 +1,52 @@
+﻿Imports TemplateDB
+
+Module Variables
+    Public OverClass As OverClass
+    Private Const TablePath As String = "\\hvivo.sharepoint.com@SSL\DavWWWRoot\hSite\Systems\Availability.accdb"
+    Private Const PWord As String = "RetroRetro*1"
+    Private Const Connect2 As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & TablePath & ";Jet OLEDB:Database Password=" & PWord
+    Private Const UserTable As String = "[Users]"
+    Private Const UserField As String = "Username"
+    Private Const AuditTable As String = "[Audit]"
+    Private Contact As String = "Daniel Ramsay"
+    Public Const SolutionName As String = "Availability Tracker"
+    Public Bank As Boolean = False
+    Public StaffID As Long = 0
+
+    Public Function GetTheConnection() As String
+        GetTheConnection = Connect2
+    End Function
+
+
+    Public Sub StartUp()
+
+        OverClass = New TemplateDB.OverClass
+        OverClass.SetPrivate(UserTable,
+                                   UserField,
+                                   Nothing,
+                                   Contact,
+                                   Connect2,
+                                   AuditTable)
+
+        OverClass.LoginCheck()
+
+        OverClass.AddAllDataItem(Form1)
+
+        Dim dt As DataTable = OverClass.TempDataTable("SELECT Bank, StaffID FROM Users WHERE UserName='" & OverClass.GetUserName & "'")
+
+        If dt.Rows.Count = 0 Then
+            MsgBox("An error occurred.")
+            Application.Exit()
+        End If
+
+        Bank = dt.Rows(0).Item(0)
+        StaffID = dt.Rows(0).Item(1).ToString
+
+        If StaffID = 0 Then
+            MsgBox("An error occurred.")
+            Application.Exit()
+        End If
+
+    End Sub
+
+End Module
